7cf2c3633d3d33e1475cff958e085941e6de21a8
Thomas Smith <thsm@umich.edu>
2022-01-23 14:39:47 -0500

    Create basic template

---
+++ Template/Template.cs
@@ -0,0 +1,24 @@
+﻿using Modding;
+
+namespace Template;
+
+public class Template : Mod, ITogglableMod
+{
+    public Template() : base(nameof(Template))
+    {
+    }
+
+    public override void Initialize()
+    {
+        Log("Initializing...");
+    }
+
+    public void Unload() {
+        Log("Unloading...");
+    }
+
+    public override string GetVersion()
+    {
+        return "0.1";
+    }
+}
